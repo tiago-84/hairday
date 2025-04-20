cbfb076347d06269667552784b19e8921f64e2ae
Tiago Nascimento <sawadeekrap1@gmail.com>
2025-04-19 21:13:38 -0300

    second commit

---
+++ src/services/schedule-new.js
@@ -0,0 +1,20 @@
+import { apiConfig } from './api-config.js';
+
+export async function scheduleNew({ id, name, when }) {
+  try {
+    //Fazendo a requisição para enviar os dados do agendamento.
+    await fetch(`${apiConfig.baseURL}/schedules`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ id, name, when }),
+    });
+
+    //Exibe mensagem de agendamento realizado.
+    alert('Agendamento realizado com sucesso!');
+  } catch (error) {
+    console.log(error);
+    alert('Não foi possivel agendar. Tente novamente mais tarde');
+  }
+}
