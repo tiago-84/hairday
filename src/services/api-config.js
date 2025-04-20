d68cacb9d603f1f584699866249a7216c0d415c1
Tiago Nascimento <sawadeekrap1@gmail.com>
2025-04-19 21:33:18 -0300

    third commit

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,3 +1,4 @@
-export const apiConfig = {
-  baseURL: 'http://localhost:3333',
-};
+const api = axios.create({
+  baseURL: '/api',
+});
+
---
+++ src/services/api/schedules.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+      res.status(200).json([
+        { id: 1, nome: 'Agendamento A', horario: '09:00' },
+        { id: 2, nome: 'Agendamento B', horario: '11:00' },
+        { id: 3, nome: 'Agendamento C', horario: '15:00' },
+      ]);
+    } else {
+      res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  }
+  
